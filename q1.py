6bb1191b7afa0ba3682d4c4ba4ddb39e9269ef6d
Gerson Jatobá <gersonmjl@gmail.com>
2022-07-07 14:27:27 -0300

    Finalizado

--- q1.py
+++ q1.py
@@ -3,35 +3,95 @@
 # the sequence.
 
 import random
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
-# Função que gera uma sequência aleatória de bits
-def generate_random_sequence(number_of_bits: int):
+# Função que gera uma sequência aleatória de sequences
+def generate_random_sequence(number_of_sequences: int):
 
     # Inicializa uma lista vazia
     sequence = []
 
-    # Loop que gera um número aleatório de 0 a 1 para cada número de bits e adiciona na lista
-    for i in range(number_of_bits):
-        sequence.append(str(random.randint(0, 1)))
+    multiplier = number_of_sequences // 2
 
-    # Transforma a lista em uma string
+    number_one = "1" * multiplier
+    number_zero = "0" * multiplier
+
+    sequence = number_one + number_zero
+    sequence = list(sequence)
+
+    # Shuffle the string
+    random.shuffle(sequence)
+
     sequence = "".join(sequence)
 
-    # Verifica se a sequência gerada é válida e a retorna
-    if sequence.count("0") == 7 and sequence.count("1") == 7:
-        return sequence
-
-    # Caso não seja válida, chama a função novamente
-    return generate_random_sequence(number_of_bits)
+    return sequence
 
 
-def main():
-    # Gera uma sequência aleatória de 14 bits
-    sequence = generate_random_sequence(14)
+def create_directed_graph(sequence: str, sequences: list):
+
+    # Removendo o primeiro digito de cada sequência
+    for i in range(len(sequences)):
+        sequences[i] = sequences[i][1:]
+
+    # Criando um grafo direcionado
+    G = nx.DiGraph()
+
+    # Criando uma lista com os arcos do grafo, onde o primeiro elemento é o nó de origem e o seguinte é o nó de destino
+    arcs = []
+    i = 0
+    while len(arcs) <= len(sequences):
+        if i == 0:
+            arcs.append((sequences[i], sequences[i]))
+            i += 1
+            continue
+        if i == len(sequences) - 1:
+            temp_tuple = (sequences[i], sequences[0])
+            if temp_tuple in arcs:
+                return main(n)
+            arcs.append(temp_tuple)
+            break
+        temp_tuple = (sequences[i], sequences[i + 1])
+        if temp_tuple in arcs:
+            return main(n)
+        arcs.append(temp_tuple)
+        i += 1
+
+    # Adicionando os arcos ao grafo
+    i = 0
+    for arc in arcs:
+        G.add_edge(arc[0], arc[1])
+        i += 1
+
+    if len(G.edges()) != 2**n:
+        return main(n)
+
+    print(sequence)
+    print(len(G.edges()))
+    print(G.edges())
+
+    pos = nx.circular_layout(G)
+
+    nx.draw_networkx_nodes(G, pos, node_size=500)
+    nx.draw_networkx_edges(G, pos, arrows=True)
+    nx.draw_networkx_labels(G, pos, font_size=10, font_family="sans-serif")
+
+    print("O grafo é euleriano? R: " + str(nx.is_eulerian(G)))
+
+    plt.box(False)
+    plt.show()
+
+    return
+
+
+def main(n):
+    number_of_sections = 2**n
+    # Gera uma sequência aleatória de 2**n bits
+    sequence = generate_random_sequence(number_of_sections)
 
     # Verifica se a sequência gerada é válida
-    if len(sequence) != 14:
+    if len(sequence) != 2**n:
         print("Sequence must be 14 characters long.")
         return
 
@@ -46,26 +106,18 @@ def main():
             tmp_arr.append(sequence[j])
             j = (j + 1) % len(sequence)
         if tmp_arr not in sequences:
-            sequences.append(tmp_arr)
+            tmp_str = "".join(tmp_arr)
+            sequences.append(tmp_str)
 
-    # Se o tamanho da lista for diferente de 14 bits, significa que alguma sequência de 4 bits está duplicada
+    # Se o tamanho da lista for diferente de 2**n bits, significa que alguma sequência de n bits está duplicada
     # e chama novamente a função até que todas as sequências sejam únicas
-    if len(sequences) != 14:
-        return main()
+    if len(sequences) != 2**n:
+        return main(n)
 
-    diff_array = [["0", "0", "0", "0"], ["1", "1", "1", "1"]]
-
-    # Caso exista sequências 0000 e 1111, apague da lista
-    if diff_array[0] in sequences:
-        sequences.remove(["0", "0", "0", "0"])
-    if diff_array[1] in sequences:
-        sequences.remove(["1", "1", "1", "1"])
-
-    print(sequence)
-    for reading in sequences:
-        print(reading)
+    create_directed_graph(sequence, sequences)
 
     return
 
 
-main()
+n = int(input("Enter value of n: "))
+main(n)
--- q2.py
+++
@@ -1,3 +0,0 @@
-# Let S be an alphabet of n letters; Show that there is a circular
-# sequence containing n^3 copies of each letter such that every fourletter 'word' formed from letters of S appears as a block of the
-# sequence.
